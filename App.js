39d944adaf306339b12a3609c697c75575318e62
kachilala <kachilala23@gmail.com>
2022-02-07 07:30:57 +0100

    complete auth with custom theme and components

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-
+import React from 'react'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { theme } from './src/core/theme'
 import SplashScreen from 'react-native-splash-screen';
 
-const App = () => {
+import {  
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+} from './src/screens'
 
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  
   React.useEffect(() => {
     SplashScreen.hide();
   }, []);
-
+  
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  );
-};
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />         
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
 
-export default App;
+// function BottomNavigation() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen
+//         name="Home"
+//         component={HomeScreen}
+//         options={{
+//           tabBarIcon: ({ color }) => <HomeIcon fill={color} />,
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Profile"
+//         component={ProfileScreen}
+//         options={{
+//           tabBarIcon: ({ color }) => <ProfileIcon fill={color} />,
+//         }}
+//       />
+//     </Tab.Navigator>
+//   )
+// }
 
+// function DrawerNavigator() {
+//   return (
+//     <Drawer.Navigator drawerContent={DrawerContent}>
+//       <Drawer.Screen name="HomeScreen" component={BottomNavigation} />
+//     </Drawer.Navigator>
+//   )
+// }
